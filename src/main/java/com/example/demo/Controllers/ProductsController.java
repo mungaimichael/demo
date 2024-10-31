d393a75886f318912e79e835e3d1b55e90256132
mungaimichael <mungaimikey@gmail.com>
2024-10-31 17:51:54 +0300

    refactor

--- src/main/java/com/example/demo/Controllers/ProductsController.java
+++ src/main/java/com/example/demo/Controllers/ProductsController.java
@@ -1,8 +1,7 @@
-package com.example.demo;
+package com.example.demo.Controllers;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
